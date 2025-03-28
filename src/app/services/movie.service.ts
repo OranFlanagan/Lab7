79ed66b771c0f23e1ba2039c670a5678253a9072
OranFlanagan <163148816+OranFlanagan@users.noreply.github.com>
2025-03-28 14:01:06 +0000

    first commit

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  constructor(private http: HttpClient) { }
 
-  constructor(private http:HttpClient) { }
-
-  getMovieData():Observable<any>
-  {
+  // Method to fetch movie data
+  // Returns an Observable of type 'any' to handle the API response
+  getMovieData(): Observable<any> {
+    // Making a GET request to the OMDb API
+    // Searching for movies with 'war' in the title
+    // Note: API key is hardcoded (in a real app, this should be secured)
     return this.http.get('https://www.omdbapi.com/?apikey=2ee2ed84&s=%27war%27');
   }
 }
